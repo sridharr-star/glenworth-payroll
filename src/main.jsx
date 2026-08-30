6ad000eb1284411b67cba69b84d035277195fa72
sridharr-star <sridhar.r@o3mdm.com>
2026-08-30 15:00:58 +0530

    Wrap GlenworthPayrollApp with AuthGate component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import GlenworthPayrollApp from "./GlenworthPayroll.jsx";
-
+import AuthGate from "./authGate.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GlenworthPayrollApp />
+    <AuthGate>
+      <GlenworthPayrollApp />
+    </AuthGate>
   </React.StrictMode>
 );
