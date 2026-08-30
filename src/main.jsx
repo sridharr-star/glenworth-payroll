6dd1354f1dba9824920c45b51af1eafc49ffe9eb
sridharr-star <sridhar.r@o3mdm.com>
2026-08-30 15:02:06 +0530

    Fix import case for AuthGate component

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import GlenworthPayrollApp from "./GlenworthPayroll.jsx";
-import AuthGate from "./authGate.jsx";
+import AuthGate from "./AuthGate.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthGate>
